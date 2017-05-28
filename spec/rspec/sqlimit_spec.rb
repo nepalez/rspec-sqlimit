c8127fa8e9093615c224a9edfb7874f4423bc693
NikiTikiTavi93 <alerto_93@mail.ru>
2017-05-28 11:25:22 +0300

    Add specs for different cases

--- spec/rspec/sqlimit_spec.rb
+++ spec/rspec/sqlimit_spec.rb
@@ -31,5 +31,48 @@ describe "exceed_query_limit" do
     it 'works when nil is used' do
       expect { User.create id: nil }.to exceed_query_limit(0).with(/INSERT/)
     end
+
+    context 'for collection' do
+      before do
+        3.times { User.create }
+      end
+      it 'works when actual number of queries exceeds the limit' do
+        expect { User.all.to_a }.to exceed_query_limit(0).with(/SELECT/)
+      end
+
+      it 'works when actual number of queries is below the limit' do
+        expect { User.all.to_a }.not_to exceed_query_limit(1).with(/SELECT/)
+      end
+
+      it 'works when array is used as a restrcition' do
+        expect { User.where(id: [1, 2, 3]).to_a }.to exceed_query_limit(0)
+                                                         .with(/SELECT/)
+      end
+
+      it 'works when array is used as a restrcition negative case' do
+        expect { User.where(id: [1, 2, 3]).to_a }.not_to exceed_query_limit(1)
+                                                         .with(/SELECT/)
+      end
+    end
+
+    context 'update or delete queries' do
+      let(:user) { User.create }
+      describe '.update' do
+        it 'works when actual number of queries is exceeds the limit' do
+          expect { user.update(id: 2) }.to exceed_query_limit(0).with(/UPDATE/)
+        end
+
+        it 'works when actual number of queries is below the limit' do
+          expect { user.update(id: 2) }.not_to exceed_query_limit(1)
+                                                   .with(/UPDATE/)
+        end
+      end
+
+      describe '.destroy' do
+        it 'works when actual number of queries is below the limit' do
+          expect { user.destroy }.not_to exceed_query_limit(0).with(/UPDATE/)
+        end
+      end
+    end
   end
 end
